927423dc2ec7d70c7165e22c17c90946f7e0b4c0
eitanv <eitanv@gmail.com>
2025-03-03 16:49:03 +0200

    Initial commit

---
+++ src/main/java/com/peer39/URLClassifier/UrlClassifierApplication.java
@@ -0,0 +1,12 @@
+package com.peer39.URLClassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlClassifierApplication.class, args);
+	}
+}
